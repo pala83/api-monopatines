86905ba5bf452b498fbc8ca6a8cbc837ee61e832
Ulises <ulipalazzoo@gmail.com>
2025-10-26 10:49:57 -0300

    estructura del proyecto

---
+++ monopatines/src/main/java/practico/integrador/monopatines/MonopatinesApplication.java
@@ -0,0 +1,13 @@
+package practico.integrador.monopatines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopatinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopatinesApplication.class, args);
+	}
+
+}
---
+++ monopatines/src/test/java/practico/integrador/monopatines/MonopatinesApplicationTests.java
@@ -0,0 +1,13 @@
+package practico.integrador.monopatines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopatinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
