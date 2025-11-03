ffe545601929caabffcf73f7c2db02031af0cf51
Rodrigo <rodrigodinelli01@gmail.com>
2025-11-03 12:53:18 -0300

    mejoro estructura del proyecto, faltan cambios seguro

---
+++ monopatines/services/trip_service/src/main/java/practico/integrador/trip_service/TripServiceApplication.java
@@ -0,0 +1,13 @@
+package practico.integrador.trip_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripServiceApplication.class, args);
+	}
+
+}
---
+++ monopatines/services/vehicle_service/src/test/java/practico/integrador/vehicle_service/VehicleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package practico.integrador.vehicle_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
