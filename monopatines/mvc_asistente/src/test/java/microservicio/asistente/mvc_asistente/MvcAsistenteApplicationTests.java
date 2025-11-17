1efaeadd3b23c1f981c68330a975a993cfbc3671
Ulises <ulipalazzoo@gmail.com>
2025-11-17 16:27:07 -0300

    Estructura mvc_asistente con IA

---
+++ monopatines/mvc_asistente/src/test/java/microservicio/asistente/mvc_asistente/MvcAsistenteApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicio.asistente.mvc_asistente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcAsistenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
